8f53a7a2816b8fb23ab32b7f0d71fc029409655a
Mikołaj Sałek <mikolaj2703@gmail.com>
2023-05-24 17:02:10 +0200

    baza + uklad buttonow

--- kalkulator/Form1.Designer.cs
+++ kalkulator/Form1.Designer.cs
@@ -28,12 +28,525 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            prosty = new Button();
+            naukowy = new Button();
+            rad = new Button();
+            deg = new Button();
+            sin = new Button();
+            inv = new Button();
+            Cos = new Button();
+            pi = new Button();
+            tan = new Button();
+            e = new Button();
+            button11 = new Button();
+            root = new Button();
+            log = new Button();
+            ln = new Button();
+            silnia = new Button();
+            jeden = new Button();
+            cztery = new Button();
+            siedem = new Button();
+            lnawias = new Button();
+            dwa = new Button();
+            piec = new Button();
+            osiem = new Button();
+            pnawias = new Button();
+            trzy = new Button();
+            szesc = new Button();
+            dziewiec = new Button();
+            procent = new Button();
+            odejmowanie = new Button();
+            mnozenie = new Button();
+            dzielenie = new Button();
+            ac = new Button();
+            dodawanie = new Button();
+            rowna = new Button();
+            dot = new Button();
+            zero = new Button();
+            xdoy = new Button();
+            exp = new Button();
+            ans = new Button();
+            display = new TextBox();
+            SuspendLayout();
+            // 
+            // prosty
+            // 
+            prosty.Location = new Point(100, 12);
+            prosty.Name = "prosty";
+            prosty.Size = new Size(237, 39);
+            prosty.TabIndex = 0;
+            prosty.Text = "Kalkulator prosty";
+            prosty.UseVisualStyleBackColor = true;
+            prosty.Click += prosty_Click;
+            // 
+            // naukowy
+            // 
+            naukowy.Location = new Point(362, 12);
+            naukowy.Name = "naukowy";
+            naukowy.Size = new Size(237, 39);
+            naukowy.TabIndex = 1;
+            naukowy.Text = "Kalkulator naukowy";
+            naukowy.UseVisualStyleBackColor = true;
+            naukowy.Click += naukowy_Click;
+            // 
+            // rad
+            // 
+            rad.Location = new Point(11, 123);
+            rad.Name = "rad";
+            rad.Size = new Size(100, 40);
+            rad.TabIndex = 2;
+            rad.Text = "Rad";
+            rad.UseVisualStyleBackColor = true;
+            rad.Click += rad_Click;
+            // 
+            // deg
+            // 
+            deg.Location = new Point(117, 123);
+            deg.Name = "deg";
+            deg.Size = new Size(100, 40);
+            deg.TabIndex = 3;
+            deg.Text = "Deg";
+            deg.UseVisualStyleBackColor = true;
+            deg.Click += deg_Click;
+            // 
+            // sin
+            // 
+            sin.Location = new Point(117, 169);
+            sin.Name = "sin";
+            sin.Size = new Size(100, 40);
+            sin.TabIndex = 5;
+            sin.Text = "Sin";
+            sin.UseVisualStyleBackColor = true;
+            sin.Click += sin_Click;
+            // 
+            // inv
+            // 
+            inv.Location = new Point(11, 169);
+            inv.Name = "inv";
+            inv.Size = new Size(100, 40);
+            inv.TabIndex = 4;
+            inv.Text = "Inv";
+            inv.UseVisualStyleBackColor = true;
+            inv.Click += inv_Click;
+            // 
+            // Cos
+            // 
+            Cos.Location = new Point(117, 215);
+            Cos.Name = "Cos";
+            Cos.Size = new Size(100, 40);
+            Cos.TabIndex = 7;
+            Cos.Text = "Cos";
+            Cos.UseVisualStyleBackColor = true;
+            Cos.Click += Cos_Click;
+            // 
+            // pi
+            // 
+            pi.Location = new Point(11, 215);
+            pi.Name = "pi";
+            pi.Size = new Size(100, 40);
+            pi.TabIndex = 6;
+            pi.Text = "π";
+            pi.UseVisualStyleBackColor = true;
+            pi.Click += pi_Click;
+            // 
+            // tan
+            // 
+            tan.Location = new Point(117, 261);
+            tan.Name = "tan";
+            tan.Size = new Size(100, 40);
+            tan.TabIndex = 9;
+            tan.Text = "Tan";
+            tan.UseVisualStyleBackColor = true;
+            tan.Click += tan_Click;
+            // 
+            // e
+            // 
+            e.Location = new Point(11, 261);
+            e.Name = "e";
+            e.Size = new Size(100, 40);
+            e.TabIndex = 8;
+            e.Text = "e";
+            e.UseVisualStyleBackColor = true;
+            e.Click += e_Click;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(118, 277);
+            button11.Name = "button11";
+            button11.Size = new Size(100, 40);
+            button11.TabIndex = 11;
+            button11.Text = "button11";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // root
+            // 
+            root.Location = new Point(223, 261);
+            root.Name = "root";
+            root.Size = new Size(100, 40);
+            root.TabIndex = 13;
+            root.Text = "√";
+            root.UseVisualStyleBackColor = true;
+            root.Click += root_Click;
+            // 
+            // log
+            // 
+            log.Location = new Point(223, 215);
+            log.Name = "log";
+            log.Size = new Size(100, 40);
+            log.TabIndex = 12;
+            log.Text = "log";
+            log.UseVisualStyleBackColor = true;
+            log.Click += log_Click;
+            // 
+            // ln
+            // 
+            ln.Location = new Point(223, 169);
+            ln.Name = "ln";
+            ln.Size = new Size(100, 40);
+            ln.TabIndex = 11;
+            ln.Text = "ln";
+            ln.UseVisualStyleBackColor = true;
+            ln.Click += ln_Click;
+            // 
+            // silnia
+            // 
+            silnia.Location = new Point(223, 123);
+            silnia.Name = "silnia";
+            silnia.Size = new Size(100, 40);
+            silnia.TabIndex = 10;
+            silnia.Text = "x!";
+            silnia.UseVisualStyleBackColor = true;
+            silnia.Click += silnia_Click;
+            // 
+            // jeden
+            // 
+            jeden.Location = new Point(329, 261);
+            jeden.Name = "jeden";
+            jeden.Size = new Size(100, 40);
+            jeden.TabIndex = 17;
+            jeden.Text = "1";
+            jeden.UseVisualStyleBackColor = true;
+            jeden.Click += jeden_Click;
+            // 
+            // cztery
+            // 
+            cztery.Location = new Point(329, 215);
+            cztery.Name = "cztery";
+            cztery.Size = new Size(100, 40);
+            cztery.TabIndex = 16;
+            cztery.Text = "4";
+            cztery.UseVisualStyleBackColor = true;
+            cztery.Click += cztery_Click;
+            // 
+            // siedem
+            // 
+            siedem.Location = new Point(329, 169);
+            siedem.Name = "siedem";
+            siedem.Size = new Size(100, 40);
+            siedem.TabIndex = 15;
+            siedem.Text = "7";
+            siedem.UseVisualStyleBackColor = true;
+            siedem.Click += siedem_Click;
+            // 
+            // lnawias
+            // 
+            lnawias.Location = new Point(329, 123);
+            lnawias.Name = "lnawias";
+            lnawias.Size = new Size(100, 40);
+            lnawias.TabIndex = 14;
+            lnawias.Text = "(";
+            lnawias.UseVisualStyleBackColor = true;
+            lnawias.Click += lnawias_Click;
+            // 
+            // dwa
+            // 
+            dwa.Location = new Point(435, 261);
+            dwa.Name = "dwa";
+            dwa.Size = new Size(100, 40);
+            dwa.TabIndex = 21;
+            dwa.Text = "2";
+            dwa.UseVisualStyleBackColor = true;
+            dwa.Click += dwa_Click;
+            // 
+            // piec
+            // 
+            piec.Location = new Point(435, 215);
+            piec.Name = "piec";
+            piec.Size = new Size(100, 40);
+            piec.TabIndex = 20;
+            piec.Text = "5";
+            piec.UseVisualStyleBackColor = true;
+            piec.Click += piec_Click;
+            // 
+            // osiem
+            // 
+            osiem.Location = new Point(435, 169);
+            osiem.Name = "osiem";
+            osiem.Size = new Size(100, 40);
+            osiem.TabIndex = 19;
+            osiem.Text = "8";
+            osiem.UseVisualStyleBackColor = true;
+            osiem.Click += osiem_Click;
+            // 
+            // pnawias
+            // 
+            pnawias.Location = new Point(435, 123);
+            pnawias.Name = "pnawias";
+            pnawias.Size = new Size(100, 40);
+            pnawias.TabIndex = 18;
+            pnawias.Text = ")";
+            pnawias.UseVisualStyleBackColor = true;
+            pnawias.Click += pnawias_Click;
+            // 
+            // trzy
+            // 
+            trzy.Location = new Point(541, 261);
+            trzy.Name = "trzy";
+            trzy.Size = new Size(100, 40);
+            trzy.TabIndex = 25;
+            trzy.Text = "3";
+            trzy.UseVisualStyleBackColor = true;
+            trzy.Click += trzy_Click;
+            // 
+            // szesc
+            // 
+            szesc.Location = new Point(541, 215);
+            szesc.Name = "szesc";
+            szesc.Size = new Size(100, 40);
+            szesc.TabIndex = 24;
+            szesc.Text = "6";
+            szesc.UseVisualStyleBackColor = true;
+            szesc.Click += szesc_Click;
+            // 
+            // dziewiec
+            // 
+            dziewiec.Location = new Point(541, 169);
+            dziewiec.Name = "dziewiec";
+            dziewiec.Size = new Size(100, 40);
+            dziewiec.TabIndex = 23;
+            dziewiec.Text = "9";
+            dziewiec.UseVisualStyleBackColor = true;
+            dziewiec.Click += dziewiec_Click;
+            // 
+            // procent
+            // 
+            procent.Location = new Point(541, 123);
+            procent.Name = "procent";
+            procent.Size = new Size(100, 40);
+            procent.TabIndex = 22;
+            procent.Text = "%";
+            procent.UseVisualStyleBackColor = true;
+            procent.Click += procent_Click;
+            // 
+            // odejmowanie
+            // 
+            odejmowanie.Location = new Point(647, 261);
+            odejmowanie.Name = "odejmowanie";
+            odejmowanie.Size = new Size(100, 40);
+            odejmowanie.TabIndex = 29;
+            odejmowanie.Text = "-";
+            odejmowanie.UseVisualStyleBackColor = true;
+            odejmowanie.Click += odejmowanie_Click;
+            // 
+            // mnozenie
+            // 
+            mnozenie.Location = new Point(647, 215);
+            mnozenie.Name = "mnozenie";
+            mnozenie.RightToLeft = RightToLeft.Yes;
+            mnozenie.Size = new Size(100, 40);
+            mnozenie.TabIndex = 28;
+            mnozenie.Text = "X";
+            mnozenie.UseVisualStyleBackColor = true;
+            mnozenie.Click += mnozenie_Click;
+            // 
+            // dzielenie
+            // 
+            dzielenie.Location = new Point(647, 169);
+            dzielenie.Name = "dzielenie";
+            dzielenie.Size = new Size(100, 40);
+            dzielenie.TabIndex = 27;
+            dzielenie.Text = "/";
+            dzielenie.UseVisualStyleBackColor = true;
+            dzielenie.Click += dzielenie_Click;
+            // 
+            // ac
+            // 
+            ac.Location = new Point(647, 123);
+            ac.Name = "ac";
+            ac.Size = new Size(100, 40);
+            ac.TabIndex = 26;
+            ac.Text = "AC";
+            ac.UseVisualStyleBackColor = true;
+            ac.Click += ac_Click;
+            // 
+            // dodawanie
+            // 
+            dodawanie.Location = new Point(647, 307);
+            dodawanie.Name = "dodawanie";
+            dodawanie.Size = new Size(100, 40);
+            dodawanie.TabIndex = 36;
+            dodawanie.Text = "+";
+            dodawanie.UseVisualStyleBackColor = true;
+            dodawanie.Click += dodawanie_Click;
+            // 
+            // rowna
+            // 
+            rowna.Location = new Point(541, 307);
+            rowna.Name = "rowna";
+            rowna.Size = new Size(100, 40);
+            rowna.TabIndex = 35;
+            rowna.Text = "=";
+            rowna.UseVisualStyleBackColor = true;
+            rowna.Click += rowna_Click;
+            // 
+            // dot
+            // 
+            dot.Location = new Point(435, 307);
+            dot.Name = "dot";
+            dot.Size = new Size(100, 40);
+            dot.TabIndex = 34;
+            dot.Text = ".";
+            dot.UseVisualStyleBackColor = true;
+            dot.Click += dot_Click;
+            // 
+            // zero
+            // 
+            zero.Location = new Point(329, 307);
+            zero.Name = "zero";
+            zero.Size = new Size(100, 40);
+            zero.TabIndex = 33;
+            zero.Text = "0";
+            zero.UseVisualStyleBackColor = true;
+            zero.Click += zero_Click;
+            // 
+            // xdoy
+            // 
+            xdoy.Location = new Point(223, 307);
+            xdoy.Name = "xdoy";
+            xdoy.Size = new Size(100, 40);
+            xdoy.TabIndex = 32;
+            xdoy.Text = "x^y";
+            xdoy.UseVisualStyleBackColor = true;
+            xdoy.Click += xdoy_Click;
+            // 
+            // exp
+            // 
+            exp.Location = new Point(117, 307);
+            exp.Name = "exp";
+            exp.Size = new Size(100, 40);
+            exp.TabIndex = 31;
+            exp.Text = "Exp";
+            exp.UseVisualStyleBackColor = true;
+            exp.Click += exp_Click;
+            // 
+            // ans
+            // 
+            ans.Location = new Point(11, 307);
+            ans.Name = "ans";
+            ans.Size = new Size(100, 40);
+            ans.TabIndex = 30;
+            ans.Text = "Ans";
+            ans.UseVisualStyleBackColor = true;
+            ans.Click += ans_Click;
+            // 
+            // display
+            // 
+            display.Location = new Point(100, 70);
+            display.Name = "display";
+            display.Size = new Size(499, 27);
+            display.TabIndex = 37;
+            display.TextChanged += display_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(761, 405);
+            Controls.Add(display);
+            Controls.Add(dodawanie);
+            Controls.Add(rowna);
+            Controls.Add(dot);
+            Controls.Add(zero);
+            Controls.Add(xdoy);
+            Controls.Add(exp);
+            Controls.Add(ans);
+            Controls.Add(odejmowanie);
+            Controls.Add(mnozenie);
+            Controls.Add(dzielenie);
+            Controls.Add(ac);
+            Controls.Add(trzy);
+            Controls.Add(szesc);
+            Controls.Add(dziewiec);
+            Controls.Add(procent);
+            Controls.Add(dwa);
+            Controls.Add(piec);
+            Controls.Add(osiem);
+            Controls.Add(pnawias);
+            Controls.Add(jeden);
+            Controls.Add(cztery);
+            Controls.Add(siedem);
+            Controls.Add(lnawias);
+            Controls.Add(root);
+            Controls.Add(log);
+            Controls.Add(ln);
+            Controls.Add(silnia);
+            Controls.Add(tan);
+            Controls.Add(e);
+            Controls.Add(Cos);
+            Controls.Add(pi);
+            Controls.Add(sin);
+            Controls.Add(inv);
+            Controls.Add(deg);
+            Controls.Add(rad);
+            Controls.Add(naukowy);
+            Controls.Add(prosty);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button prosty;
+        private Button naukowy;
+        private Button rad;
+        private Button deg;
+        private Button sin;
+        private Button inv;
+        private Button Cos;
+        private Button pi;
+        private Button tan;
+        private Button e;
+        private Button button11;
+        private Button root;
+        private Button log;
+        private Button ln;
+        private Button silnia;
+        private Button jeden;
+        private Button cztery;
+        private Button siedem;
+        private Button lnawias;
+        private Button dwa;
+        private Button piec;
+        private Button osiem;
+        private Button pnawias;
+        private Button trzy;
+        private Button szesc;
+        private Button dziewiec;
+        private Button procent;
+        private Button odejmowanie;
+        private Button mnozenie;
+        private Button dzielenie;
+        private Button ac;
+        private Button dodawanie;
+        private Button rowna;
+        private Button dot;
+        private Button zero;
+        private Button xdoy;
+        private Button exp;
+        private Button ans;
+        private TextBox display;
     }
 }
--- kalkulator/Form1.cs
+++ kalkulator/Form1.cs
@@ -6,5 +6,177 @@ namespace kalkulator
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void prosty_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void deg_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void trzy_Click(object sender, EventArgs e)
+        {
+            display.Text += 3;
+        }
+
+        private void procent_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dodawanie_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rowna_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dot_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void zero_Click(object sender, EventArgs e)
+        {
+            display.Text += 0;
+        }
+
+        private void xdoy_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void exp_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void ans_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void odejmowanie_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void mnozenie_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void dzielenie_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void ac_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void szesc_Click(object sender, EventArgs e)
+        {
+            display.Text += 6;
+        }
+
+        private void dziewiec_Click(object sender, EventArgs e)
+        {
+            display.Text += 9;
+        }
+
+        private void dwa_Click(object sender, EventArgs e)
+        {
+            display.Text += 2;
+        }
+
+        private void piec_Click(object sender, EventArgs e)
+        {
+            display.Text += 5;
+        }
+
+        private void osiem_Click(object sender, EventArgs e)
+        {
+            display.Text += 8;
+        }
+
+        private void pnawias_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void jeden_Click(object sender, EventArgs e)
+        {
+            display.Text += 1;
+        }
+
+        private void cztery_Click(object sender, EventArgs e)
+        {
+            display.Text += 4;
+        }
+
+        private void siedem_Click(object sender, EventArgs e)
+        {
+            display.Text += 7;
+        }
+
+        private void lnawias_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void root_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void log_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void ln_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void silnia_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void tan_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void e_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Cos_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void pi_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void sin_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void inv_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void rad_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void naukowy_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void display_TextChanged(object sender, EventArgs e)
+        {
+        }
     }
 }
